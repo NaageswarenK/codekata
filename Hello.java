c4e9fc0fe461bd7b586379045011cc15cb9ef9bd
NaageswarenK <naageshwaren1996@gmail.com>
2017-06-20 16:28:30 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class Vowel {
+class Hello {
 	public static void main(String[] args){
 		for(int i=0;i<5;i++)
 			System.out.println("Hello");
