f83cb12906df88bcab26b544c848a7f774bfc97d
NaageswarenK <naageshwaren1996@gmail.com>
2017-06-20 16:43:49 +0530

    Create naturalNumbers.java

---
+++ naturalNumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class naturalNumbers {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number of digits");
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=1;i<=n;i++)
+			sum+=i;
+		System.out.println("Sum of "+n+" natural numbers is "+sum);
+	}
+}
