0dc5fb7f2f77bc572128445df96bb1d6607af54e
NaageswarenK <naageshwaren1996@gmail.com>
2017-06-20 16:47:23 +0530

    Update naturalNumbers.java

--- naturalNumbers.java
+++ naturalNumbers.java
@@ -4,7 +4,7 @@ import java.io.*;
 class naturalNumbers {
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the Number of digits");
+		System.out.println("Enter the Number of terms");
 		int n=sc.nextInt();
 		int sum=0;
 		for(int i=1;i<=n;i++)
