a15d0451e8ef0c377756eb2067dc2e9d83a07b39
NaageswarenK <naageshwaren1996@gmail.com>
2017-06-22 15:15:37 +0530

    Update primeRange.java

--- primeRange.java
+++ primeRange.java
@@ -7,13 +7,8 @@ class primeRange
 	{	
 		int a[]=new int[2];int flag=0,count=0,i=0;
 		Scanner sc=new Scanner(System.in);
-		String str=sc.nextLine();
-		StringTokenizer scan=new StringTokenizer(str,"-");
-		while(scan.hasMoreTokens()){
-			String temp=scan.nextToken();
-			a[i]=Integer.parseInt(temp);
-			i++;
-		}
+		a[0]=sc.nextInt();
+		a[1]=sc.nextInt();
 		for(i=a[0];i<=a[1];i++){
 			flag=0;
 			for(int j=2;j<i;j++){
